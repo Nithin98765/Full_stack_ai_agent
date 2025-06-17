9ca140904b381223ff3301cf814ca2d032df82c4
NITHIN B M <nithinbmpadthare90@gmail.com>
2025-06-17 14:31:55 +0530

    added index file

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 import express from "express"
 import mongoose from "mongoose"
 import cors from "cors"
+
+const PORT=process.env.PORT || 3000
+
+const app =express()
+
+app.use(cors())
+app.use(express.json())
+
+if (!process.env.MONGO_URI) {
+    console.error("Error: MONGO_URI environment variable is not set.");
+    process.exit(1);
+}
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(()=>{
+        console.log("MongoDB Connected");
+        app.listen(PORT,()=>console.log(`Server at http://localhost:${PORT}`))
+    })
+    .catch((err)=> console.error("MongoDB error:",err))
+
+
